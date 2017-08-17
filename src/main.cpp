16f24bc44fed20735adc4df9463e086d79665549
Adam Obuchowicz <aobuchowicz@flowbox.io>
2017-08-17 16:33:49 +0200

    Do not include libnode.so to qrc
    It is embed in exec in the same way as lunaInstall exec

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,28 @@
 #include <iostream>
 #include <cassert>
 
+const auto InstallerAppDirName = "lunaInstallerApp";
+
+// these path are relative to qrc root or temporary directory
+const auto InstallerExecPath = "lunaInstallerApp/lunaInstaller";
+const auto ConsoleInstallerExecPath = "lunaInstallerApp/resources/app/consoleInstaller";
+const auto LibNodeDllPath = "lunaInstallerApp/libnode.so";
+
+enum class EmbeddedFiles {
+	LunaInstallerExec, LibNodeDll
+};
+
 #ifndef _WIN32
 extern char _binary_lunaInstaller_start[];
 extern char _binary_lunaInstaller_end[];
+extern char _binary_libnode_so_start[];
+extern char _binary_libnode_so_end[];
+
+const std::map<EmbeddedFiles, std::pair<char *, char *>> pointersToEmbeddedFile =
+{
+	{ EmbeddedFiles::LunaInstallerExec, { _binary_lunaInstaller_start, _binary_lunaInstaller_end } },
+	{ EmbeddedFiles::LibNodeDll, { _binary_libnode_so_start, _binary_libnode_so_end } }
+};
 #endif
 
 #define RUNTIME_ERROR(Name,description)                                     \
@@ -21,7 +40,8 @@ extern char _binary_lunaInstaller_end[];
 
 RUNTIME_ERROR(CreateTemporaryDirectoryError, "Could not create temporary directory");
 RUNTIME_ERROR(ExtractingFileError, "Error while extracting luna installer files to temporary directory");
-RUNTIME_ERROR(ExtractingDirecotryError, "Error while extracting luna installer directory to temporary directory");
+RUNTIME_ERROR(ExtractingDirectoryError, "Error while extracting luna installer directory to temporary directory");
+RUNTIME_ERROR(ExtractingFromExecError, "Error while extracting embedded file");
 RUNTIME_ERROR(SettingPermissionError, "Error while setting execute permission on extracted luna installer");
 
 
@@ -30,7 +50,7 @@ void recursiveCopy(const QDir &copiedDir, const QDir &destination)
     assert(copiedDir.exists());
 
     if (!destination.mkdir(copiedDir.dirName()))
-        throw ExtractingDirecotryError();
+		throw ExtractingDirectoryError();
     QDir newDir(destination.absoluteFilePath(copiedDir.dirName()));
 
     for (auto &file : copiedDir.entryInfoList(QDir::Files | QDir::Dirs | QDir::NoDotAndDotDot | QDir::Hidden | QDir::System))
@@ -46,34 +66,39 @@ void recursiveCopy(const QDir &copiedDir, const QDir &destination)
 }
 
 
-void extractExecutable(const char *fileName)
+void extractFromExecutable(EmbeddedFiles fileId, const QString &fileName)
 {
     QFile file(fileName);
 #ifdef _WIN32
     static_assert(false, "Michale, wymyśl tutaj coś mądrego");
 #else
-    if (!file.open(QFile::WriteOnly))
-        throw ExtractingFileError();
 
-    file.write(_binary_lunaInstaller_start, _binary_lunaInstaller_end - _binary_lunaInstaller_start);
+    if (!file.open(QFile::WriteOnly))
+		throw ExtractingFromExecError();
+
+	auto dataPtrs = pointersToEmbeddedFile.at(fileId);
+	file.write(dataPtrs.first, dataPtrs.second - dataPtrs.first);
 #endif
 }
 
-QString extractLunaInstaller(QTemporaryDir &tmpDir,
-                             const char *installerAppDirName,
-                             const char *installerExecPath)
+QString extractLunaInstaller(QTemporaryDir &tmpDir)
 {
-
     const QDir destinationDir(tmpDir.path());
-    const QString execPath = tmpDir.filePath(installerExecPath);
-    const QString installerAppDirPath = QString(":/") + installerAppDirName;
+
+	const QString extractedInstallerExecPath = tmpDir.filePath(InstallerExecPath);
+	const QString extractedLibNodeDllPath = tmpDir.filePath(LibNodeDllPath);
+
+	const QString installerAppDirPath = QString(":/") + InstallerAppDirName;
     const QDir installerAppDir(installerAppDirPath);
 
     std::cout << "Extracting files to " << destinationDir.path().toStdString() << std::endl;
-    recursiveCopy(installerAppDir, destinationDir);
-    extractExecutable(execPath.toStdString().c_str());
+	recursiveCopy(installerAppDir, destinationDir);
+	extractFromExecutable(EmbeddedFiles::LunaInstallerExec,
+						  extractedInstallerExecPath);
+	extractFromExecutable(EmbeddedFiles::LibNodeDll,
+						  extractedLibNodeDllPath);
 
-    return execPath;
+	return extractedInstallerExecPath;
 }
 
 template<class... Paths>
@@ -93,8 +118,8 @@ int main(int argc, char *argv[])
 {
     QTemporaryDir tmpDir;
     tmpDir.setAutoRemove(false);
-    auto execPath = extractLunaInstaller(tmpDir, "lunaInstallerApp", "lunaInstallerApp/lunaInstaller");
-	setExecPermissionsInFiles(tmpDir, "lunaInstallerApp/lunaInstaller", "lunaInstallerApp/resources/app/consoleInstaller");
+	auto execPath = extractLunaInstaller(tmpDir);
+	setExecPermissionsInFiles(tmpDir, InstallerExecPath, ConsoleInstallerExecPath, LibNodeDllPath);
 
     QProcess process;
     process.start(execPath);
